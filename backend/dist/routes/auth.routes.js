3a826cc161308872d1795876ed5ae3e026915d07
BikramSingh6589 <bikramsb314@gmail.com>
2026-06-10 21:39:11 +0530

    Deleted

--- backend/dist/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { authenticateStudent, getAuthenticatedStudent, loginStudent, registerStudent, } from "../controllers/auth.controller.js";
-const router = Router();
-router.post("/register", registerStudent);
-router.post("/login", loginStudent);
-router.get("/me", authenticateStudent, getAuthenticatedStudent);
-export default router;
-//# sourceMappingURL=auth.routes.js.map
